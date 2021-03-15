28b8bbd98dbc86f1fb7c0188f69fcf0520677cdd
Matt Yu <lwlmm_77@163.com>
2021-03-16 03:43:27 +0800

    Latest version of source code

---
+++ Cuckoo/src/sizeofeverything.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    printf("Size of int    is %ld\n",sizeof(int));
+    printf("Size of char   is %ld\n",sizeof(char));
+    printf("Size of short  is %ld\n",sizeof(short));
+    printf("Size of long   is %ld\n",sizeof(long));
+    printf("Size of float  is %ld\n",sizeof(float));
+    printf("Size of double is %ld\n",sizeof(double));
+
+    return 0;
+}
